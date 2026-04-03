5df920266a4ddcdde133e2258fdc9c5314451950
kuzRK <sanalsogdanov11188@gmail.com>
2026-04-03 20:37:38 +0300

    note hpp

---
+++ sogdanov.sanal/T1/note.hpp
@@ -0,0 +1,13 @@
+
+#define NOTE_HPP
+#include <string>
+#include <vector>
+#include <memory>
+namespace sogdanov {
+  struct Note {
+    std::string name;
+    std::vector<std::string> lines;
+    std::vector<std::weak_ptr<Note>> links;
+  };
+}
+#endif 
