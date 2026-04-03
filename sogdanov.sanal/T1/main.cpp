8346ab246c8f38cbb9c8dfdd9cef974398b905c3
kuzRK <sanalsogdanov11188@gmail.com>
2026-04-03 21:16:54 +0300

    main implementation

--- sogdanov.sanal/T1/main.cpp
+++ sogdanov.sanal/T1/main.cpp
@@ -1,4 +1,34 @@
 #include <iostream>
-
-int main()
-{}
+#include <string>
+#include <unordered_map>
+#include <limits>
+#include "commands.hpp"
+int main() {
+  using cmd_t = void(*)(std::istream&, std::ostream&);
+  std::unordered_map<std::string, cmd_t> cmds;
+  cmds["note"] = sogdanov::cmd_note;
+  cmds["line"] = sogdanov::cmd_line;
+  cmds["show"] = sogdanov::cmd_show;
+  cmds["drop"] = sogdanov::cmd_drop;
+  cmds["link"] = sogdanov::cmd_link;
+  cmds["halt"] = sogdanov::cmd_halt;
+  cmds["mind"] = sogdanov::cmd_mind;
+  cmds["expired"] = sogdanov::cmd_expired;
+  cmds["refresh"] = sogdanov::cmd_refresh;
+  std::string cmd;
+  while (std::cin >> cmd) {
+    try {
+      cmds.at(cmd)(std::cin, std::cout);
+    } catch (const std::out_of_range &) {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } catch (const std::logic_error &) {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+  if (!std::cin.eof()) {
+    std::cerr << "Bad input\n";
+    return 1;
+  }
+}
